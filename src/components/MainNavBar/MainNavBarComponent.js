179db2d8dc23c4757655bace3fed0280528cbabc
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-07-05 20:04:26 -0300

    Fix Color and Font Styling

--- src/components/MainNavBar/MainNavBarComponent.js
+++ src/components/MainNavBar/MainNavBarComponent.js
@@ -5,8 +5,8 @@ import './mainNavBarStyles.scss';
 class MainNavBarComponent extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky">
-                <a className="navbar-brand color-hover-link" href="/">FracBeer</a>
+            <nav className="navbar navbar-expand-lg navbar-dark sticky">
+                <a className="navbar-brand color-hover-link" href="/">Frac Beer</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
