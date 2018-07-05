5362357aff8d28534669f158df80d141668bd07a
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-07-05 17:55:31 -0300

    Add More Header and MainNavBar tests

--- test/components/Header/HeaderComponent.test.js
+++ test/components/Header/HeaderComponent.test.js
@@ -1,7 +1,7 @@
 import 'jsdom-global/register';
 import React from 'react';
 import { expect } from 'chai';
-import { mount, shallow } from 'enzyme'
+import { mount, shallow, render } from 'enzyme'
 
 import HeaderComponent from '../../../src/components/Header/HeaderComponent';
 
@@ -15,6 +15,13 @@ describe('HeaderComponent', () => {
     it('should have children', () => {
         const wrapper = mount(<HeaderComponent />);
         expect(wrapper.children()).to.exist;
+    })
+
+    it('should render title correctly', () => {
+        const wrapper = mount(<HeaderComponent />);
+
+        const title = wrapper.find('h1').text();
+        expect(title).to.equal('Frac Beer');
     })
 
 });
---
+++ test/components/MainNavBar/MainNavBarComponent.test.js
@@ -0,0 +1,20 @@
+import 'jsdom-global/register';
+import React from 'react';
+import { expect } from 'chai';
+import { mount, shallow } from 'enzyme'
+
+import MainNavBarComponent from '../../../src/components/MainNavBar/MainNavBarComponent';
+
+describe('MainNavBarComponent', () => {
+
+    it('should exist', () => {
+        const wrapper = shallow(<MainNavBarComponent />);
+        expect(wrapper).to.exist;
+    });
+
+    it('should have children', () => {
+        const wrapper = mount(<MainNavBarComponent />);
+        expect(wrapper.children()).to.exist;
+    })
+
+});
